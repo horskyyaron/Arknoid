12040de66cec30d80713378d74e33359188a6cf1
horskyyaron <57840832+horskyyaron@users.noreply.github.com>
2020-05-05 18:16:38 +0300

    Ass3Game passed

--- Ass3Game.java
+++ Ass3Game.java
@@ -1,5 +1,18 @@
+//ID: 204351670
+
+/**
+ * Ass3Game class will initialize and run Arkanoid game.
+ */
 public class Ass3Game {
 
+    /**
+     * main function.
+     * will create a new Arkanoid game, initialize it and run it.
+     *
+     * @param args input from user. (will not impact the program)
+     * @throws Exception if one of the game objects will have a point with
+     *                   negative coordinates.
+     */
     public static void main(String[] args) throws Exception {
         Game game = new Game();
         game.initialize();
