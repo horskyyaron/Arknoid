e55770ef2f4c24f6dcaf5a00fbd1f5a4b9f42836
horskyyaron <57840832+horskyyaron@users.noreply.github.com>
2020-05-05 14:09:56 +0300

    CollisionInfo passed

--- CollisionInfo.java
+++ CollisionInfo.java
@@ -1,27 +1,55 @@
+//ID: 204351670
+
+/**
+ * CollisionInfo class supports methods that their goal is to represent
+ * a collision that happen in the game.
+ */
 public class CollisionInfo {
 
     //fields
     private Point collisionPoint;
     private Collidable collisionObject;
 
+    /**
+     * constructor of the 'CollisionInfo' object.
+     *
+     * @param collisionPoint the point which the collision took place in.
+     * @param collisionObject the Collidable object that is involved with the
+     *                        collision.
+     */
     public CollisionInfo(Point collisionPoint, Collidable collisionObject) {
         this.collisionPoint = collisionPoint;
         this.collisionObject = collisionObject;
     }
 
-    // the point at which the collision occurs.
+    /**
+     * returns the collision point.
+     *
+     * @return the collision point.
+     */
     public Point collisionPoint() {
         return this.collisionPoint;
     }
 
-    // the collidable object involved in the collision.
+    /**
+     * returns the Collidable object that is involved in the collision..
+     *
+     * @return the Collidable object that is involved in the collision..
+     */
     public Collidable collisionObject() {
         return this.collisionObject;
     }
 
+    /**
+     * calculate which of the Collidable's lines was a part of the collision,
+     * then returns that line.
+     *
+     * @return the line which is involved in the collision.
+     * @throws Exception if the collision point has a negative coordinate.
+     */
     public Line getCollisionImpactLine() throws Exception {
 
-        return (this.collisionObject.getImpactLineFromCollisionPoint
-                (this.collisionPoint));
+        return (this.collisionObject.getImpactLineFromCollisionPoint(this.
+                collisionPoint));
     }
 }
